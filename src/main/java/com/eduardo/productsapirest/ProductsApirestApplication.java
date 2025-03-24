c868e5a16d3cfed0aeabca73bdaff82f87650d36
Eduardo XVlll <88681044+EduardoHead18@users.noreply.github.com>
2025-03-24 17:05:49 -0600

    change:  docker compose down command before starting services and update server startup message with public IP

--- src/main/java/com/eduardo/productsapirest/ProductsApirestApplication.java
+++ src/main/java/com/eduardo/productsapirest/ProductsApirestApplication.java
@@ -9,6 +9,6 @@ public class ProductsApirestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsApirestApplication.class, args);
 		//test develop branch
-		System.out.println("Server start on 🚀 : http://localhost:8080/swagger-ui/index.html#/ ");
+		System.out.println("Server start on 🚀: http://3.142.35.109:8080/swagger-ui/index.html#/ ");
 	}
 }
